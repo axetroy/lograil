332c9b894d69ddb7ec7877d4d12556ac53cf9257
axetroy <axetroy@users.noreply.github.com>
2026-09-06 07:58:30 +0000

    docs: update root to 6cb5d2f0814e77ed74560d2ffd022fc421146295 6cb5d2f0814e77ed74560d2ffd022fc421146295

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.Cf8muYHb.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.DlYDG6cq.js";import{_ as a}from"../app.D0YBNPmr.js";import"./chunk-5VM5RSS4.CRQPyj6Q.js";import"./chunk-XXDRQBXY.DK7r6zwl.js";import"./chunk-POPQ4Y6H.DNIKArCA.js";import"./chunk-F27PBJKO.CMUs2UGT.js";import"./channel.Bpqtm2IO.js";import"./framework.BzWmN0d8.js";import"./theme.BpOGCDBI.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
